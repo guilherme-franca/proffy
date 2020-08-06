c35be245621e798550271a4770b859bd29cbb6f3
Guilherme Franca <guilherme-f-f@hotmail.com>
2020-08-06 08:46:56 -0300

    Terceiro dia | Third day

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: string;
+    name: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    subject: string;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function  createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/17651443?s=460&u=52faaf0e05c8f52a74c5a30ff1c0c13339c5fd94&v=4" alt="Guilherme"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Guilherme</strong>
-                    <span>Quimica</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de quimica avançada.
-                <br /><br />
-                Apaixonado por explodir ciosas em laboratório e por mudar a vida das pessoas através de experiencia.
-            </p>
+            
+            <p>{teacher.bio}</p>
+
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80.00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
